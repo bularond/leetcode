a137a0cd637eef9e294cbb6e8300b5f878d53cf0
Bulat Akhmetzyanov <bularond@protonmail.com>
2025-01-21 15:29:02 +0300

    2145-GRID-GAME Stats: Time: 0 ms (100%), Space: 3.4 MB (75%) - LeetHub

--- 2145-grid-game/2145-grid-game.rs
+++ 2145-grid-game/2145-grid-game.rs
@@ -1,28 +1,16 @@
-fn sum(arr: &[i64], l: usize, r: usize) -> i64 {
-    if r == 0 || l >= r {
-        0
-    } else if l == 0 {
-        arr[r - 1]
-    } else {
-        arr[r - 1] - arr[l - 1]
-    }
-}
 
 impl Solution {
-    pub fn grid_game(grid: Vec<Vec<i32>>) -> i64 {    
+    pub fn grid_game(grid: Vec<Vec<i32>>) -> i64 {
         let n = grid[0].len();
-        let mut first_sum = vec![0_i64; n];
-        let mut second_sum = vec![0_i64; n];
-        first_sum[0] = grid[0][0] as i64;
-        second_sum[0] = grid[1][0] as i64;
-        for i in 1..n {
-            first_sum[i] = first_sum[i - 1] + grid[0][i] as i64;
-            second_sum[i] = second_sum[i - 1] + grid[1][i] as i64;
-        }
+        let sum1: i64 = grid[0].iter().map(|x| *x as i64).sum();
     
+        let mut cur1 = sum1;
+        let mut cur2 = 0;
         let mut min = i64::MAX;
         for i in 0..n {
-            min = min.min(sum(&first_sum, i + 1, n).max(sum(&second_sum, 0, i)));
+            cur1 -= grid[0][i] as i64;
+            min = min.min(cur1.max(cur2));
+            cur2 += grid[1][i] as i64;
         }
     
         min
